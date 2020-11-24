aa095be2486f75a1037053eedfdd26019d37a431
ulaş karakaya <ulas_gs_11@hotmail.com>
2020-11-24 13:40:58 +0300

    Entities and Context files were created.

---
+++ API/DAL/Context/ProjectContext.cs
@@ -0,0 +1,20 @@
+﻿using API.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.DAL.Context
+{
+    public class ProjectContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=WebAPIDb;Trusted_Connection=True;");
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+    }
+}
---
+++ API/DAL/Entities/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.DAL.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Blog> Blogs { get; set; }
+    }
+}
