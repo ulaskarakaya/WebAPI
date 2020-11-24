fc11b160d69daebf9b75772cdf61b239070fe22c
ulaş karakaya <ulas_gs_11@hotmail.com>
2020-11-24 14:27:04 +0300

    The web layer that will connect with the api were opened.

---
+++ UI.Web/Controllers/HomeController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using API.DAL.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using UI.Web.Models;
+
+namespace UI.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            using var httpClient = new HttpClient();
+            //httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("", "");
+
+            var responseMessage = await httpClient.GetAsync("http://localhost:60230/api/categories");
+
+            var jsonString = await responseMessage.Content.ReadAsStringAsync();
+            var categories = JsonConvert.DeserializeObject<List<Category>>(jsonString);
+            return View(categories);
+        }
+
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Category category)
+        {
+            var httpClient = new HttpClient();
+            var jsonCategory = JsonConvert.SerializeObject(category);
+            StringContent content = new StringContent(jsonCategory, Encoding.UTF8, "application/json");
+            var responseMessage = await httpClient.PostAsync("http://localhost:60230/api/categories", content);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+
+            ModelState.AddModelError("", "bir sorun oluştu");
+            return View(category);
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var httpClient = new HttpClient();
+            var responseMessage = await httpClient.GetAsync("http://localhost:60230/api/categories/" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonCategory = await responseMessage.Content.ReadAsStringAsync();
+
+                var category = JsonConvert.DeserializeObject<Category>(jsonCategory);
+
+                return View(category);
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Category category)
+        {
+            using var client = new HttpClient();
+            var content = new StringContent(JsonConvert.SerializeObject(category), Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("http://localhost:60230/api/categories/", content);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            ModelState.AddModelError("", "Server tarafında bir hata oluştu");
+            return View(category);
+        }
+    }
+}
